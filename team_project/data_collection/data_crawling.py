2317121c7b5bb19e6be1c713d8838562d5c7f526
lovehyun2001 <lovehyun2001@naver.com>
2025-02-03 10:51:16 +0900

    Add files via upload

---
+++ team_project/data_collection/data_crawling.py
@@ -0,0 +1,21 @@
+# cmd 창 => conda activate py39
+# pip install bing-image-downloader
+# 비쥬얼 코드 오른쪽 하단에서 py39 환경으로 변경
+import os
+import glob
+from bing_image_downloader import downloader
+
+def download_images(keyword, limit):
+    downloader.download(keyword, limit=limit, output_dir='images', adult_filter_off=True, force_replace=False)
+
+    # 다운로드된 폴더 경로
+    download_path = os.path.join("images", keyword)
+    
+    # .jpg가 아닌 파일 삭제
+    for file in glob.glob(os.path.join(download_path, "*")):
+        if not file.lower().endswith(".jpg"):
+            os.remove(file)
+
+keyword = input("이미지 검색어를 입력하세요: ")
+limit = int(input("다운로드할 이미지 수를 입력하세요: "))
+download_images(keyword, limit)
